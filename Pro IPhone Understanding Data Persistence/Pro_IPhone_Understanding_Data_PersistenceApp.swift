5567cf196a8ba0b976588d8ffebac6ffae3a24e2
neddington <neddington@cascadia.edu>
2023-11-01 16:37:43 -0700

    Initial Commit

---
+++ Pro IPhone Understanding Data Persistence/Pro_IPhone_Understanding_Data_PersistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_IPhone_Understanding_Data_PersistenceApp.swift
+//  Pro IPhone Understanding Data Persistence
+//
+//  Created by Eddington, Nick on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Pro_IPhone_Understanding_Data_PersistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
