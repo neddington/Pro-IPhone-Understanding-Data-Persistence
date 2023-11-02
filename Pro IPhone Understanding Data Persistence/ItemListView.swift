150b27f760ea0feb33f589f382a6358fca128383
neddington <neddington@cascadia.edu>
2023-11-01 17:28:20 -0700

    Fin

---
+++ Pro IPhone Understanding Data Persistence/ItemListView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemListView.swift
+//  Pro IPhone Understanding Data Persistence
+//
+//  Created by Eddington, Nick on 11/1/23.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemListView()
+}
---
+++ Pro IPhone Understanding Data Persistence/PersistenceController.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceController.swift
+//  Pro IPhone Understanding Data Persistence
+//
+//  Created by Eddington, Nick on 11/1/23.
+//
+
+import Foundation
